bf2295269267b07e69dd59995f2c2b21b0db1d11
Richard Cobbe <ricobbe@microsoft.com>
2021-07-06 10:14:02 -0700

    Add test case for multiple inconsistent definitions of an extern function

---
+++ src/test/ui/rfc-2627-raw-dylib/multiple-definitions.rs
@@ -0,0 +1,17 @@
+// only-windows-msvc
+// compile-flags: --crate-type lib
+#![feature(raw_dylib)]
+#![allow(clashing_extern_declarations, incomplete_features)]
+// #[link(name = "foo", kind = "raw-dylib")]
+// extern "C" {
+//     fn f(x: i32);
+// }
+
+pub fn lib_main() {
+    #[link(name = "foo", kind = "raw-dylib")]
+    extern "stdcall" {
+        fn f(x: i32);
+    }
+
+    unsafe { f(42); }
+}
